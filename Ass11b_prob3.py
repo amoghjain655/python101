2d4a48104a17e9e4867be2b7ed94dc30417e6831
amoghjain655 <62167159+amoghjain655@users.noreply.github.com>
2021-07-15 12:57:05 -0700

    Add files via upload

---
+++ Ass11b_prob3.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, new_data):
+        self.data = new_data
+        self.next = None
+     
+class LinkedList:
+    def __init__(self):
+        self.head = None
+ 
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+ 
+
+    def printNthFromLast(self, n):
+        temp = self.head
+         
+        length = 0
+        while temp is not None:
+            temp = temp.next
+            length += 1
+         
+        if n > length:
+            
+            print('Location is greater than the' +
+                         ' length of LinkedList')
+            return
+        temp = self.head
+        for i in range(0, length - n):
+            temp = temp.next
+        print(temp.data)
+ 
+      
+llist = LinkedList()
+llist.push("A")
+llist.push("B")
+llist.push("C")
+llist.push("D")
+llist.push("E")
+llist.push("F")
+llist.printNthFromLast(3,1)
